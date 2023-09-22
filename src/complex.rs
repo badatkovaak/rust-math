5e6ae84526007626548e20d7cb5d4f049082f5d6
badatkovaak <122653634+badatkovaak@users.noreply.github.com>
2023-09-22 10:33:49 +0000

    !

--- src/complex.rs
+++ src/complex.rs
@@ -1,5 +1,19 @@
-pub type complex = (f64,f64);
+pub type ComplexAlg = (f64,f64);
+pub type ComplexPolar = (f64,f64);
 
-pub fn add(c1: Complex, c2: Complex) -> Complex {
+pub fn add(c1: ComplexAlg, c2: ComplexAlg) -> ComplexAlg {
     return (c1[0] + c2[0],c1[1] + c2[1])
+}
+
+pub fn conjugate(c: &ComplexAlg) -> ComplexAlg {
+    return (c[0], -c[1])
+}
+
+pub fn algebraic_to_polar(c: &ComplexAlg) -> ComplexPolar {
+    let r = sqrt(c[0]*c[0] + c[1]*c[1]);
+    return (c[0]/r,c[1]/r)
+}
+
+pub fn polar_to_algebraic(c: &ComplexPolar) -> ComplexAlg {
+    return ()
 }
--- src/matrix.rs
+++ src/matrix.rs
@@ -9,7 +9,19 @@ pub fn scalar_mult(m: &Matrix, s: f64) -> Matrix {
         .collect();
 }
 
+pub fn scalar_mult2(m1: &Matrix2, s: f64) -> Matrix2 {
+    return m.map(|x| x.map(|y| y * s ))
+}
 
+pub fn add2(m1: &Matrix2 , m2: &Matrix2) -> Matrix2 {
+    return m1
+        .iter()
+        .zip(m2.iter())
+        .map(|(x,y)| x.iter().zip(y.iter()).map(|(x1,y1)| x1 + y1).collect::<Vec<f64>>().try_into().unwrap())
+        .collect::<Vec<[f64;2]>>()
+        .try_into()
+        .unwrap()
+}
 
 pub fn add(m1: &Matrix, m2: &Matrix) -> Matrix {
     return m1
