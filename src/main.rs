1c28a71e4c7d63617e1fa1b92346e95563f42873
badatkovaak <122653634+badatkovaak@users.noreply.github.com>
2023-10-08 22:22:56 +0300

    1

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,24 @@
 pub mod complex;
 pub mod equations;
 pub mod matrix;
+pub mod matrix2;
 pub mod utils;
 pub mod vector;
+
+// use matrix::Matrix;
 
 fn main() {
     // let vec1: Vec<f64> = vec![1., 2., 3.];
     // let vec2: Vec<f64> = vec![4., 5., 6.];
     // let vec12 = vector::add(&vec1, &vec2);
-    let s1 = utils::sqrt_doom(2_000_000_000_000.0);
-    let s2 = utils::sqrt_force(2_000_000_000_000.0);
+    // let s1 = utils::sqrt_doom(2_000_000_000_000.0);
+    // let s2 = utils::sqrt_force(2_000_000_000_000.0);
     // let s3 = utils::one_over_sqrt(2000000.0);
     // let m1 = vec![vec![1., 2.], vec![3., 4.]];
-    // let m2 =
+    let m2 = matrix::get_id_matrix(3);
     // let x = matrix::scalar_mult(&m1, 3.);
-    println!("{} {} {}", s2, 1, s1);
+    // println!("{} {} {}", s2, 1, s1);
+    println!("{:?}", m2.append_row(vec![1.0, 2.0, 3.0]));
     // println!("{:?}", matrix::add(&m1, &x));
     // println!("{:?}", vector::scalar_mult(&vec1, 10.));
 }
--- src/matrix.rs
+++ src/matrix.rs
@@ -1,139 +1,133 @@
 use std::slice::{Iter, IterMut};
 
-pub type Matrix2 = [[f64; 2]; 2];
-// pub type Matrix3 = [[f64; 3]; 3];
-// pub type Matrix = Vec<Vec<f64>>;
+#[derive(Debug)]
 pub struct Matrix(Vec<Vec<f64>>);
 
 impl Iterator for Matrix {
     type Item = Vec<f64>;
+
     fn next(&mut self) -> Option<Self::Item> {
-        return self.0.iter().next().cloned();
+        self.0.iter().next().cloned()
     }
 }
 
 impl FromIterator<Vec<f64>> for Matrix {
     fn from_iter<T: IntoIterator<Item = Vec<f64>>>(iter: T) -> Self {
-        return Self(Vec::from_iter(iter));
+        Self(Vec::from_iter(iter))
+    }
+}
+
+impl std::ops::Neg for Matrix {
+    type Output = Matrix;
+
+    fn neg(self) -> Self::Output {
+        self.scalar_mult(-1.0)
     }
 }
 
 impl std::ops::Add for Matrix {
-    type Output = Matrix;
+    type Output = Self;
 
     fn add(self, rhs: Self) -> Self::Output {
-        return self::Matrix::myadd(self, rhs);
+        // return self::Matrix::myadd(self, rhs);
+        self.iter()
+            .zip(rhs.iter())
+            .map(|(x, y)| x.iter().zip(y.iter()).map(|(x1, y1)| x1 + y1).collect())
+            .collect()
     }
 }
 
+impl std::ops::Sub for Matrix {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        self + (-rhs)
+    }
+}
+
+impl std::ops::Mul for Matrix {
+    type Output = Option<Self>;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        if self.0[0].len() != rhs.0.len() {
+            return None;
+        }
+        let mut res = vec![vec![0.0; self.0.len()]; rhs.0[0].len()];
+        for i in 0..self.0.len() {
+            for k in 0..rhs.0[0].len() {
+                for j in 0..rhs.0.len() {
+                    res[i][j] += self.0[i][k] * rhs.0[k][j];
+                }
+            }
+        }
+        return Some(Matrix(res));
+    }
+}
+
+// impl std::ops::Div for Matrix {
+//     type Output = Option<Self>;
+//
+//     fn div(self, rhs: Self) -> Self::Output {}
+// }
+
 impl Matrix {
     pub fn iter(&self) -> Iter<'_, Vec<f64>> {
-        return self.0.iter();
+        self.0.iter()
     }
 
     pub fn iter_mut(&mut self) -> IterMut<'_, Vec<f64>> {
-        return self.0.iter_mut();
+        self.0.iter_mut()
     }
 
     pub fn to_vec(&mut self) -> Self {
-        return Self(self.0.to_vec());
+        Self(self.0.to_vec())
     }
 
     pub fn scalar_mult(self, s: f64) -> Matrix {
-        return self
-            .iter()
+        self.iter()
             .map(|x| x.iter().map(|y| y * s).collect())
-            .collect();
+            .collect()
     }
 
-    pub fn scalar_mult2(m: &Matrix2, s: f64) -> Matrix2 {
-        return m.map(|x| x.map(|y| y * s));
+    // pub fn myadd(self, m2: Matrix) -> Matrix {
+    //     return self
+    //         .iter()
+    //         .zip(m2.iter())
+    //         .map(|(x, y)| x.iter().zip(y.iter()).map(|(x1, y1)| x1 + y1).collect())
+    //         .collect();
+
+    pub fn transpose(mut self) -> Matrix {
+        for i in 0..self.0.len() {
+            for j in 0..self.0[i].len() {
+                self.0[i][j] = self.0[j][i];
+            }
+        }
+        return self;
     }
 
-    pub fn add2(m1: &Matrix2, m2: &Matrix2) -> Matrix2 {
-        return m1
-            .iter()
-            .zip(m2.iter())
-            .map(|(x, y)| {
-                x.iter()
-                    .zip(y.iter())
-                    .map(|(x1, y1)| x1 + y1)
-                    .collect::<Vec<f64>>()
-                    .try_into()
-                    .unwrap()
-            })
-            .collect::<Vec<[f64; 2]>>()
-            .try_into()
-            .unwrap();
-    }
-
-    pub fn myadd(self, m2: Matrix) -> Matrix {
-        return self
-            .iter()
-            .zip(m2.iter())
-            .map(|(x, y)| x.iter().zip(y.iter()).map(|(x1, y1)| x1 + y1).collect())
-            .collect();
-    }
-
-    pub fn make_into_step_form(m: &mut Matrix) -> Matrix {
+    pub fn make_into_step_form(&mut self) -> Matrix {
         todo!()
         // return *m;
     }
 
-    pub fn append_row(m: &mut Matrix, row: Vec<f64>) -> Matrix {
-        for i in m.iter_mut() {
-            i.push(row[0]);
-            // <Vec<f64> as AsMut<Vec<f64>>>::as_mut(i);
+    // pub fn get_inverse(&mut self) -> Self {}
+
+    pub fn append_row(mut self, row: Vec<f64>) -> Matrix {
+        for i in 0..row.len() {
+            self.0[i].push(row[i]);
         }
-        return m.to_vec();
+        return self;
     }
 }
-//
-// pub fn scalar_mult(m: &Matrix, s: f64) -> Matrix {
-//     return m
-//         .iter()
-//         .map(|x| x.iter().map(|y| y * s).collect())
-//         .collect();
-// }
-//
-// pub fn scalar_mult2(m: &Matrix2, s: f64) -> Matrix2 {
-//     return m.map(|x| x.map(|y| y * s));
-// }
-//
-// pub fn add2(m1: &Matrix2, m2: &Matrix2) -> Matrix2 {
-//     return m1
-//         .iter()
-//         .zip(m2.iter())
-//         .map(|(x, y)| {
-//             x.iter()
-//                 .zip(y.iter())
-//                 .map(|(x1, y1)| x1 + y1)
-//                 .collect::<Vec<f64>>()
-//                 .try_into()
-//                 .unwrap()
-//         })
-//         .collect::<Vec<[f64; 2]>>()
-//         .try_into()
-//         .unwrap();
-// }
-//
-// pub fn add(m1: Matrix, m2: Matrix) -> Matrix {
-//     return m1
-//         .iter()
-//         .zip(m2.iter())
-//         .map(|(x, y)| x.iter().zip(y.iter()).map(|(x1, y1)| x1 + y1).collect())
-//         .collect();
-// }
-//
-// pub fn make_into_step_form(m: &mut Matrix) -> Matrix {
-//     todo!()
-//     // return *m;
-// }
-//
-// pub fn append_row(m: &mut Matrix, row: Vec<f64>) -> Matrix {
-//     for i in m.iter_mut() {
-//         i.push(row[0]);
-//         // <Vec<f64> as AsMut<Vec<f64>>>::as_mut(i);
-//     }
-//     return m.to_vec();
-// }
+
+pub fn get_id_matrix(dim: u32) -> Matrix {
+    let mut res = vec![vec![0.0; dim as usize]; dim as usize];
+    for i in 0..dim {
+        res[i as usize][i as usize] = 1.0;
+    }
+    return Matrix(res);
+}
+
+pub fn scalar_to_matrix(value: f64, dim: u32) -> Matrix {
+    get_id_matrix(dim).scalar_mult(value)
+}
