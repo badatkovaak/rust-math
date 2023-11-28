21062fa7fc42585a77f7c07c928e32d71d763d87
badatkovaak <122653634+badatkovaak@users.noreply.github.com>
2023-11-28 13:18:31 +0000

    1

--- src/complex_nums/c_polar.rs
+++ src/complex_nums/c_polar.rs
@@ -1,7 +1,7 @@
 #[derive(Debug, Clone, Copy)]
 pub struct CPolar(pub f64, pub f64);
 
-use crate::complex_nums::c_algebraic;
+use crate::{complex_nums::c_algebraic, constants::PI};
 use std::ops;
 
 impl ops::Neg for CPolar {
@@ -29,8 +29,17 @@ impl ops::Div for CPolar {
 }
 
 impl CPolar {
+
     fn polar_to_algebraic(z: CPolar) -> c_algebraic::CAlgebraic {
         let c = z.1.sin_cos();
         c_algebraic::CAlgebraic(z.0 * c.1, z.0 * c.0)
+    }
+
+    fn n_th_roots(self, n: u64) -> Vec<CPolar> {
+        let mut res = vec![];
+        for i in 0..n {
+            res.push(CPolar(self.0.powf(1./(i as f64)), (self.1 + 2.*PI)/(i as f64)));
+        }
+        res
     }
 }
--- src/equations.rs
+++ src/equations.rs
@@ -13,3 +13,10 @@ pub fn quadratic_roots(a: f64, b: f64, c: f64) -> Vec<f64> {
         (-b * utils::sqrt_force(d) / a),
     ];
 }
+
+// pub fn cubic_roots(a: f64, b: f64, c: f64, d: f64) -> Vec<f64> {
+//     let d0 = b*b - 3. * a * c;
+//     let d1 = 2. * b*b*b - 9.*a*b*c + 27.*a*a*a*d;
+//     let res = vec![];
+//     res
+// }
